637ac5f39dfbe53e89d2bb369b47d61dd04d31b1
terencelimzhengwei <terencelimz@gmail.com>
2016-02-26 17:18:37 +0800

    finalised server.R and ui.R

--- server.R
+++ server.R
@@ -5,30 +5,87 @@ library(plotly)
 library(dplyr)
 library(tidyr)
 
+########## Function to load long term gym data from csv########################
 
+loadLongData <- function(){
+    print("Loading Long Term Data")
+    return (read.csv("./chartGym.csv",header=TRUE,colClasses = c("factor","numeric","factor","numeric")))
+}
 
-longData <-read.csv("./chartGym.csv",header=TRUE,colClasses = c("factor","numeric","factor","numeric"))
+########## Load Long Term data ###############################################
 
+longData <- loadLongData()
+
+###################################################
+
+######## Open Database Connection #################
+
+con <- mongo("results",url="mongodb://public:public@ds037415.mongolab.com:37415/gym")
+
+######### List of Gym Names #######################
+
+gymNames <- c("Bishan","Bedok","Bukit Gombak","Choa Chu Kang","Clementi",
+             "Delta","Hougang","Jurong East","Jurong West","Pasir Ris",
+             "Seng Kang","Tampines","Toa Payoh", "Woodlands","Yio Chu Kang",
+             "Yishun")
+
+######## Function to retrieve latest 720 data from database ##################
+getNewData <- function(){
+    print("Getting new Data")
+    my_data <- con$find(limit=720,sort='{"$natural":-1}')
+    new_data <- my_data$results
+    print("new_data <- my_data$results")
+    new_data$time <- as.POSIXct(my_data$time,format="%a, %d %b %Y %H:%M:%S +0000")
+    print("convert time")
+    rm(my_data)
+    print("remove my data")
+    new_data <- gather(new_data,"gym","percentage",1:16)
+    print("gather data")
+    new_data$percentage<-as.numeric(sub("%", "", new_data$percentage))
+    print("convert to percentage")
+    new_data$gym <- sapply(new_data$gym,niceName)
+    print("nicename")
+    new_data$gym <- factor(new_data$gym)
+    print("factorise")
+    return (new_data)
+}
+
+######### Function to convert db gym name to presentable name ##################
+
+niceName <-function(gym){
+    new_name <-switch(as.character(gym),
+                      "bedok"="Bedok",
+                      "bishan"="Bishan",
+                      "bukit_gombak"="Bukit Gombak",
+                      "choa_chu_kang"="Choa Chu Kang",
+                      "clementi"="Clementi",
+                      "delta"="Delta",
+                      "hougang"="Hougang",
+                      "jurong_east"="Jurong East",
+                      "jurong_west"="Jurong West",
+                      "pasir_ris"="Pasir Ris",
+                      "seng_kang"="Seng Kang",
+                      "tampines"="Tampines",
+                      "toa_payoh"="Toa Payoh",
+                      "woodlands"="Woodlands",
+                      "yio_chu_kang"="Yio Chu Kang",
+                      "yishun"="Yishun")
+    return(new_name)
+}
+
+######## Start Shiny Server ###################################################
 shinyServer(function(input,output,session){
-    con <- mongo("results",url="mongodb://public:public@ds037415.mongolab.com:37415/gym")
     
-    getNewData <- function(){
-        my_data <- con$find(limit=720,sort='{"$natural":-1}')
-        new_data <- my_data$results
-        new_data$time <- as.POSIXct(my_data$time,format="%a, %d %b %Y %H:%M:%S +0000")
-        rm(my_data)
-        new_data <- gather(new_data,"gym","percentage",1:16)
-        new_data$percentage<-as.numeric(sub("%", "", new_data$percentage))
-        new_data$gym <- sapply(new_data$gym,niceName)
-        new_data$gym <- factor(new_data$gym)
-        return (new_data)
-    }
     
+    ### Retrieve latest 720 data every 60 seconds ###
     getReactiveData <- reactive({
         invalidateLater(60000,session)
+        print("Retrieving new data")
         getNewData()})
     
+    ### From latest 720 data, return only the row with the most current timing ##
     getAllLatestData<- reactive({
+        print("Getting Latest new Data")
         new_data <- getReactiveData()
         latestTime <- max(new_data$time)
         new_data1 <- new_data %>%
@@ -37,71 +94,76 @@ shinyServer(function(input,output,session){
         new_data1
     })
     
-    niceName <-function(gym){
-        new_name <-switch(as.character(gym),
-                          "bedok"="Bedok",
-                          "bishan"="Bishan",
-                          "bukit_gombak"="Bukit Gombak",
-                          "choa_chu_kang"="Choa Chu Kang",
-                          "clementi"="Clementi",
-                          "delta"="Delta",
-                          "hougang"="Hougang",
-                          "jurong_east"="Jurong East",
-                          "jurong_west"="Jurong West",
-                          "pasir_ris"="Pasir Ris",
-                          "seng_kang"="Seng Kang",
-                          "tampines"="Tampines",
-                          "toa_payoh"="Toa Payoh",
-                          "woodlands"="Woodlands",
-                          "yio_chu_kang"="Yio Chu Kang",
-                          "yishun"="Yishun")
-        return(new_name)
-    }
+    ## Get the relevant data required for     ##
+    ## realtime data analysis based on inputs ##
     
+    getComparisonPlotData <- reactive({
+        print("Getting Comparison Plot Data")
+        validate(
+            need(input$gym1, "Waiting for inputs to register..."),
+            need(input$gym2, "Please wait while graph is plotted....")
+        )
+        new_data <- filter(getReactiveData(),gym==input$gym1|gym==input$gym2)
+    })
+    
+    ## Filter long term data based on inputs
+    getLongTermData <- reactive ({
+        print("Filtering longterm data based on inputs")
+        validate(
+            need(input$longGym, "Waiting for inputs to register..."),
+            need(input$longDay, "Please wait while graph is plotted....")
+        )
+        longData[longData$gym==input$longGym&longData$day==input$longDay,]
+    })
+    
+    ## Render real time comparison plot ##
     output$comparisonPlot <- renderPlotly({
-        new_data <- getReactiveData()
-        new_data <- filter(new_data,gym==input$gym1|gym==input$gym2)
-        g<-ggplot(new_data,aes(time,percentage,col=gym))+geom_line(size=0.01)+ggtitle(paste(input$gym1,"vs",input$gym2))
+        print("Plotting comparison plot real time")
+        g<-ggplot(getComparisonPlotData(),aes(time,percentage,col=gym))+geom_line(size=0.01)+ggtitle(paste(input$gym1,"vs",input$gym2))
         ggplotly(g)
     })
     
+    ## Render selection for gym 1 input ##
     output$selectGym1 <- renderUI({
-        newData <- c("Bishan","Bedok","Bukit Gombak","Choa Chu Kang","Clementi",
-                     "Delta","Hougang","Jurong East","Jurong West","Pasir Ris",
-                     "Seng Kang","Tampines","Toa Payoh", "Woodlands","Yio Chu Kang",
-                     "Yishun")
+        print("Rendering select gym1")
+        newData <- gymNames
         selectInput("gym1", "First Gym", choices = newData, selected = newData[1])
     })
+    
+    ## Render selection for long term gym selection input ##
     output$longGym <- renderUI({
-        newData <- c("Bishan","Bedok","Bukit Gombak","Choa Chu Kang","Clementi",
-                     "Delta","Hougang","Jurong East","Jurong West","Pasir Ris",
-                     "Seng Kang","Tampines","Toa Payoh", "Woodlands","Yio Chu Kang",
-                     "Yishun")
-        selectInput("longGym", "Select Gym", choices = newData, selected = newData[1])
+        print("Rendering longterm gym names")
+        newData <- gymNames
+        selectInput("longGym", "Select Gym", choices = gymNames, selected = gymNames[1])
     })
     
+    ## Render selection for long term day selection input ##
     output$longDay <- renderUI({
+        print("Rendering longterm gym days")
         newData <- c("Sun","Mon","Tues","Wed","Thurs","Fri","Sat","Sun")
         selectInput("longDay", "Select Day", choices = newData, selected = newData[5])
     })
+    
+    ## Reneder selection for gym 2 for real time input ##
     output$selectGym2 <- renderUI({
-        newData <- c("Bishan","Bedok","Bukit Gombak","Choa Chu Kang","Clementi",
-                     "Delta","Hougang","Jurong East","Jurong West","Pasir Ris",
-                     "Seng Kang","Tampines","Toa Payoh", "Woodlands","Yio Chu Kang",
-                     "Yishun")
-        selectInput("gym2", "Second Gym", choices = newData, selected = newData[1])
+        print("Rendering gym2")
+        newData <- gymNames
+        selectInput("gym2", "Second Gym", choices = gymNames, selected = gymNames[1])
     })
     
+    ## Render long term plot ##
     output$longTerm <- renderPlotly({
-        newData <- longData[longData$gym==input$longGym&longData$day==input$longDay,]
-        g <- ggplot(newData,aes(hour,percentage))+geom_point(col="orange",alpha=0.04)+
+        print("Plotting long term data")
+        g <- ggplot(getLongTermData(),aes(hour,percentage))+geom_point(col="orange",alpha=0.04)+
             geom_smooth(col="black",alpha=0.4,size=0.01)+
             ggtitle(paste(input$longGym,"Capacity on",input$longDay))
         ggplotly(g)
         
     })
     
+    ## Render real time gym capacity table ##
     output$realTimeTable <- renderUI({
+        print("Rendering table")
         tableData <- getAllLatestData()
         
         tags$table(class = "table",
--- ui.R
+++ ui.R
@@ -20,9 +20,18 @@ body <- dashboardBody(
                    box(width=NULL,status="primary",title="Real Time Comparison Plots", solidHeader=TRUE,
                        plotlyOutput("comparisonPlot")
                    ),
-                   box(width=NULL,status="warning",title="Comparison Inputs",solidHeader = TRUE,
-                       uiOutput("selectGym1"),
-                       uiOutput("selectGym2")
+                   fluidRow(
+                       box(width=6,status="warning",title="Comparison Inputs",solidHeader = TRUE,
+                           uiOutput("selectGym1"),
+                           uiOutput("selectGym2")
+                       ),
+                       box(width=6,status="info",title="About",solidHeader = TRUE,
+                           p("This section shows the real time capacity of ActiveSG Gyms in Singapore (updated per minute). Please be patient as the data takes some time to load."),
+                           br(),
+                           p("If you want to compare the capacity of various gyms, please change the selection inputs accordingly. I hope this application helps you decide on the best
+                             time/location for your workout.")
+                       )
+                       
                    )
             ),
             column(4,
@@ -39,6 +48,16 @@ body <- dashboardBody(
                    )
             ),
             column(4,
+                   box(width = NULL,status = "warning", title="About", solidHeader=TRUE,
+                       p("This section shows the long term trend of gym capacity over the past few months. Please be patient as the charts takes some time to load."),
+                       br(),
+                       p("The orange points represent the actual data points collected.
+                          The darker the colour, the more frequent the occurence of that capacity."),
+                       br(),
+                       p("The black line represents the general trend of capacity based on the data."),
+                       br(),
+                       p("To view the trends of gym capacity for different gyms on different days, please change the inputs accordingly.")
+                   ),
                    box(width = NULL,status = "info", title="Inputs", solidHeader=TRUE,
                        uiOutput("longGym"),
                        uiOutput("longDay")
@@ -47,7 +66,27 @@ body <- dashboardBody(
             
         ),
         tabItem(
-            tabName="about"
+            tabName="about",
+            column(4,
+                   box(width = NULL,status = "primary", title="About me", solidHeader=TRUE,
+                       p("Summary : Just an average joe who likes to learn more about anything techy:D"),
+                       br(),
+                       p("1. Terence is what people call me."),
+                       p("2. National University of Singapore is where I come from."),
+                       p("3. Industrial and Systems Engineering is what I study.")
+                   ),
+                   box(width = NULL,status = "warning", title="Project Description", solidHeader=TRUE,
+                       p("Wouldn't it be nice if you knew when the gym was the least crowded so you could have all the weights to yourself? 
+                         That was how this whole project idea started."),
+                       br(),
+                       p("In order to analyse the gym capacity trends of ActiveSG gyms in Singapore, I created a script to collect data of ActiveSG Gym Capacity automatically
+                         using Python and running it on my handy dandy raspberry pi as a cron job."),
+                       br(),
+                       p("The data collected is then used to create the visualisations that you see on this website. This website is built using Shiny and the source code
+                          can be found at the link below"),
+                       a(href="https://github.com/terencelimzhengwei/active-sg-gym","Click here for source code!")
+                       )
+            )
         )
     )
 )
